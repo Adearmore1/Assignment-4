1dbe4fc051dbea202bdc8fa8411da0ccb9ff7050
Joseph Stanton <stantoje@sunyit.edu>
2015-11-23 00:45:09 -0500

    Initial Commit

---
+++ src/main/java/Bank.java
@@ -0,0 +1,7 @@
+/**
+ * Created by jstanton on 11/23/15.
+ */
+public class Bank {
+
+    public List<>
+}
---
+++ src/main/java/CustomCheckingAccount.java
@@ -0,0 +1,4 @@
+import com.stantonj.hca.java2015.assignment5.CheckingAccount;
+
+public class CustomCheckingAccount extends CheckingAccount {
+}

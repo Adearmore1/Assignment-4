33b6d7d2a073440284908a0b76b29e2e6a20d73d
Joseph Stanton <jsromejoe@gmail.com>
2015-12-07 12:37:55 -0500

    finished assignment 4

---
+++ README.md
@@ -0,0 +1,32 @@
+# Assignment 4
+### Description
+This assignment focuses on reinforcing the following topics:
+* Sets
+* Lists
+* if statements
+
+### Instructions
+Bank.java:
+1. Pick one of the following code snippets and insert it in the appropriate place
+2. Add alice1, bob1, and alice2 to accounts
+
+Driver.java:
+3. Create a map of strings to balances, that is <String, Double>
+4. if a's account owner isn't in the map, add a balance of 0
+5. Update the balance in balances of user a to their balance + a's balance
+6. Add i to int_set
+7. Add i to int_list
+
+### Grading
+This assignment will be graded based on completion of the following points:
+
+* No compilation errors - (4)
+* 1 - (2)
+* 2 - (2)
+* 3 - (2)
+* 4 - (2)
+* 5 - (2)
+* 6 - (2)
+* 7 - (2)
+
+Total Points: 18
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,4 @@
-import java.util.Map;
+import java.util.*;
 
 public class Driver {
     public static void main(String[] args){
@@ -17,5 +17,29 @@ public class Driver {
              * 5. Update the balance in balances of user a to their balance + a's balance
              */
         }
+
+        SortedSet<Integer> int_set = new TreeSet<>();
+        List<Integer> int_list = new ArrayList<>();
+
+        for(int i = 0; i != 5; i = (i+2)%7){
+            /**
+             * 6. Add i to int_set
+             */
+
+            /**
+             * 7. Add i to int_list
+             */
+        }
+
+        System.out.println("List output:");
+        printIterator(int_list.iterator());
+        System.out.println("\nSet output:");
+        printIterator(int_set.iterator());
+
+
+    }
+
+    public static void printIterator(Iterator<Integer> iter){
+        iter.forEachRemaining(System.out::println);
     }
 }
